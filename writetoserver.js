2a28fe98b70c373f482d3c4e64ac0381fb2c1c74
vincentabolarin <80973232+vincentabolarin@users.noreply.github.com>
2021-04-21 09:32:11 +0100

    Updated file with comments.

--- writetoserver.js
+++ writetoserver.js
@@ -1,14 +1,17 @@
+// Importing the required modules
 const http = require('http');
 const fetch = require('node-fetch');
 const fs = require('fs');
 const path = require('path');
 
+// Checking if the folder exists and creating it if it does not.
 const resultFolder = 'result';
 if (!fs.existsSync(resultFolder)) {
     fs.mkdirSync(resultFolder);
     console.log(`The directory ${resultFolder} has been successfully created.`);
 }
 
+// Fetching the data from JSON Placeholder, stringifying it, and writing it to posts.json in the created folder.
 const jsonData = fetch('http://jsonplaceholder.typicode.com/posts')
 .then(response => response.json())
 .then(jsonData => {
@@ -18,9 +21,11 @@ const jsonData = fetch('http://jsonplaceholder.typicode.com/posts')
     });
 })
 
+// Checking the path of the posts.json file.
 const fileDirectory = path.join(__dirname,'result', 'posts.json');
 const port = 5454;
 
+// Creating a server, reading the data on the checked path and rendering it on the server.
 http.createServer((req, res) => {
     fs.readFile(fileDirectory, 'utf-8', (err, data) => {
         res.writeHead(200, {'Content-Type': 'application/json'});
